4b887524047e578ba6f66dccf37d51605621b373
rprasetya <rayaprasetya90@gmail.com>
2023-11-08 22:01:26 +0800

    hubungkan bootstrap

---
+++ resources/views/base/base.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Sistem Pengajuan Spot Bak Sampah</title>
+</head>
+<body>
+    <header class="navbar bg-primary px-5">
+        <h1 class="text-black">SPSBS</h1>
+        <nav>
+            <a href="#" class="text-black">Dashboard</a>
+        </nav>
+    </header>
+    
+    @yield('content')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
