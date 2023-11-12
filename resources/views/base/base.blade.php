eba95d8c4a88937ba37fec23f917afa04c7c15c5
riyandiii <iniemailriyandi@gmail.com>
2023-11-13 03:40:19 +0800

    fix conflict base

--- resources/views/base/base.blade.php
+++ resources/views/base/base.blade.php
@@ -21,11 +21,7 @@
                     <a href="/" class="link-success link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover">Utama</a>
                     <a href="/spot-guest" class="link-success link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover">Spot</a>
                     <a href="/bantuan-guest" class="link-success link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover">Bantuan</a>
-<<<<<<< HEAD
                     <a href="/login" class="link-success link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover">Beranda</a>
-=======
-                    <a href="/spot" target="_blank" class="link-success link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover">Dasbor</a>
->>>>>>> origin/raya
                 </div>
             </div>
           </nav>
