f04739dfc8b61d92737d61140d25041ffc1242e3
rprasetya <rayaprasetya90@gmail.com>
2023-11-14 08:54:21 +0800

    dasbor bantuan

--- resources/views/user/dasbor-bantuan.blade.php
+++ resources/views/user/dasbor-bantuan.blade.php
@@ -37,7 +37,7 @@
                     <form action="/keluar" method="POST">
                         @csrf
                         <button class="nav-link text-white">
-                        Keluar
+                            Keluar
                         </button>
                     </form>
                 </li>
@@ -55,24 +55,29 @@
                 <h2 class="text-success">Pusat Bantuan</h2>
             </div>
             <div class="box">
-                <div class="contact form">
-                    <h4>Hubungi kami di :</h4>
+                <div class="contact form d-flex flex-column gap-2">
+                    <h4 class="fs-2 mt-3">Hubungi kami di :</h4>
+                    <div class="d-flex flex-column fs-4 gap-3">
+                        <a class="text-dark text-opacity-75 link-dark link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="mailto:dlh@kaltimprov.go.id">
+                            📧 dlh@kaltimprov.go.id
+                        </a>
+                        <a class="text-dark text-opacity-75 link-dark link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="mailto:kec.sungaikunjang@gmail.com">
+                            ✉️ kec.sungaikunjang@gmail.com
+                        </a>
+                        <a class="text-dark text-opacity-75 link-dark link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="https://www.google.com/search?q=nomor+telpon+dlh+sungai+kunjnag&oq=nomor+telpon+dlh+sungai+kunjnag&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIHCAEQABiiBDIHCAIQABiiBDIHCAMQABiiBDIHCAQQABiiBNIBCDc3NDVqMGoxqAIAsAIA&sourceid=chrome&ie=UTF-8#">
+                            ☎️ +(0541)743450
+                        </a>
+                    </div>
                 </div>
                 <div class="contact info">
-                    <h4>Informasi Kontak</h4>
+                    <h4>Informasi Lainnya</h4>
                     <div class="infoBox">
                         <div>
                             <span></span>
                             <p>Kec. Sungai Kunjang, Kota Samarinda, Kalimantan Timur<br>INDONESIA</p>
                         </div>
-                        <div>
-                            <span></span>
-                            <a href="mailto:dlh@kaltimprov.go.id. "></a>
-                            <a href="mailto:kec.sungaikunjang@gmail.com "></a>
-                        </div>
                     </div>
                 </div>
-                <div class="contact map"></div>
             </div>
         </div>
     </div>
